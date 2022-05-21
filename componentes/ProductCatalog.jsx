de234c6749f8ce1fa02c5c423d1fc76ac6dc9585
renato_sans <renato@datacopy.com.br>
2022-05-21 16:10:29 -0300

    Ajustes

--- componentes/ProductCatalog.jsx
+++ componentes/ProductCatalog.jsx
@@ -1,5 +1,9 @@
 
 export default function ProductCatalog({ produto = null, shoppingCart = null }) {
+    function addToCart(){
+        alert('Not implemented yet');
+    }
+
 	return (
 		<>
 			<style jsx>{`
@@ -8,8 +12,47 @@ export default function ProductCatalog({ produto = null, shoppingCart = null }) 
                     border-radius: 10px;
                     background-color:silver;
                 }
+
+                .vertical-container {
+                    position: relative;
+                    display: flex;
+                    flex-direction: column;
+                }
+            
+                .productImage {
+                    height: 250px;
+                }
+            
+                .roundIcon {
+                    border: 0px;
+                    border-radius: 50%;
+                    height: 40px;
+                    width: 40px;
+                }
+            
+                .roundIcon:hover {
+                    background-color: #FF8C00;
+                }
+            
+                .addToCart {
+                    position: absolute;
+                    bottom: 90px;
+                    right: 30px;
+                    background-image: url('icons/cart.svg');
+                    background-color: #00AAFF;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    background-size: 65%;
+                }            
 			`}</style>
-			<p><b>Produto {produto}, carrinho {shoppingCart.quantidadeItems}</b></p>
+
+            <div className='vertical-container rounded-corners'>
+                <img className='productImage' src={produto.imagem} alt={produto.nome}></img>
+                <span> {produto.nome} </span> <br/>
+                <span> {produto.descricao} </span> <br/>
+                <span> Preço: R$ {produto.preco} </span> <br/>
+                <button className='roundIcon addToCart' onClick={addToCart} />
+            </div>
 		</>
 	);
 }

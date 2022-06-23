435e92adace690a5ff1bdf375c61111bee9d17d3
renato_sans <renato@datacopy.com.br>
2022-06-23 10:48:43 -0300

    Ajustes

--- database/create.sql
+++ database/create.sql
@@ -1,15 +1,14 @@
-
 CREATE DATABASE IF NOT EXISTS commercedb;
 
-use commercedb;
+USE commercedb;
 
 CREATE TABLE IF NOT EXISTS produto (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    descricao VARCHAR(255) NULL,
-    preco DECIMAL(10,2),
-    foto LONGTEXT NOT NULL,
-    formatoImagem VARCHAR(45) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    descricao VARCHAR(255),
+    foto LONGTEXT,
+    formatoImagem VARCHAR(45),
     dataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 

0e8cfb9c6e8f921d70789d3e7646cc0ede4c4bc5
renato_sans <renato@datacopy.com.br>
2022-07-04 07:12:31 -0300

    Ajustes

--- pages/api/carrinho.js
+++ pages/api/carrinho.js
@@ -1,4 +1,4 @@
 
 export default function handler(req, res) {
-    res.status(200).json({ cliente:"John Doe", total: 0, produtosAdicionados: [] })
+    res.status(200).json({ cliente:"Lewis Hamilton", total: 0, produtosAdicionados: [] })
   }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import { Drawer, Typography } from '@mui/material';

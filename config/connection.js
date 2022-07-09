01aa763105c831583af8e4ad163116f02165d7e8
renato_sans <renato@datacopy.com.br>
2022-07-09 10:51:20 -0300

    Configuração para servidor serverless

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 import { createPool } from "mysql2/promise";
 
 
-const pool = createPool({
-	host: "localhost",
-	user: "root",
-	password: "p@ssw0rd",
-	port: 3306,
-	database: "commercedb",
-});
+// dados para conexão com banco de dados serverless
+const host = 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud';
+const username = Buffer.from('ZGttbXkzdjZ6dzZq', 'base64').toString('ascii');
+const password = Buffer.from('cHNjYWxlX3B3XzMyYlFkYVI2NWdWalg2QUJzampzUUZkVk92anhnN2JZajJONXc5bnNMSjQ=', 'base64').toString('ascii');
 
-export { pool };
+const pool = createPool({
+	host: host,
+	user: username,
+	password: password,
+	port: 3306,
+	database: 'commercedb',
+	ssl: {}
+})
+
+export { pool }

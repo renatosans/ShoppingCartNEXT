4900d6bdcb46cc8277aec5148a4e44109c879ae8
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-03-19 17:32:05 -0300

    Ajustes

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -3,15 +3,19 @@ import { Button } from '@mui/material';
 import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 
-export default function InfoModal(props) {
+type props = {
+    title: string;
+}
+
+export default function InfoModal({title, children}: React.PropsWithChildren<props>) {
     const [show, setShow] = useState(true);
     const toggle = () => setShow(!show);
 
     return  <>
             <Dialog open={show} onClose={toggle}>
-                <DialogTitle>Informação</DialogTitle>
+                <DialogTitle>Informação{title}</DialogTitle>
                 <DialogContent>
-                    <span>{props.children}</span>
+                    <span>{children}</span>
                 </DialogContent>
                 <DialogActions>
                     <Button variant="outlined" onClick={toggle}>Close</Button>
